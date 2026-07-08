1bc00ff0e671dd0187ead5d7f76da84579540c23
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-07-08 14:55:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const script = document.createElement("script");
-script.src = "https://gc.zgo.at/count.js";
-script.async = true;
-script.setAttribute(
-  "data-goatcounter",
-  "https://anmolsrivastava073.goatcounter.com/count"
-);
-
-document.head.appendChild(script);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
